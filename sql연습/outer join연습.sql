b6619813c02bcb70cd71c0033df1d3b56dee8282
PC21 <PC21@DESKTOP-MJVFQRQ>
2023-07-07 10:00:07 +0900

    outer join

---
+++ sql연습/outer join연습.sql
@@ -0,0 +1,11 @@
+-- Outer Join
+
+-- insert into dept values(null, '총무');
+-- insert into dept values(null, '영업');
+-- insert into dept values(null, '개발');
+-- insert into dept values(null, '기획');
+
+
+select * from dept;
+
+-- 
